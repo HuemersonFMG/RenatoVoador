304e9c1e5e787d490f0610e72d5ded7c6134e154
HuemersonFMG <66384987+HuemersonFMG@users.noreply.github.com>
2020-06-03 16:25:11 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1591211276,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/5moedas.png",
+		"images/aviao-sheet0.png",
+		"images/background-sheet0.png",
+		"images/calda.png",
+		"images/coletormoeda-sheet0.png",
+		"images/moeda-sheet0.png",
+		"images/pontosaviao-sheet0.png",
+		"images/pontuacao1-sheet0.png",
+		"images/predio-sheet0.png",
+		"images/renato-sheet0.png",
+		"images/renatoinicio-sheet0.png",
+		"images/sol-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
+		"images/pontospredio-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
